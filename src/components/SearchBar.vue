cd9a3b1a7d20e84af5208b853ab755cd8e1a6387
giovannimegliola <giovanni.megliola@hotmail.it>
2023-11-14 15:59:37 +0100

    added SearchBar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <select name="searchbar" id="searchbar" v-model="textSearch" @change="passData">
+        <option value="">All</option>
+        <option value="Alien">Alien</option>
+        <option value="Artifact">Artifact</option>
+        <option value="Goblin">Goblin</option>
+        <option value="Monarch">Monarch</option>
+        <option value="Pendulum">Pendulum</option>
+        <option value="Volcanic">Volcanic</option>        
+      </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  data() {
+      return {
+      textSearch: ''
+    }
+  },
+  methods: {
+    passData() {
+      this.$emit('filterChange', this.textSearch);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
